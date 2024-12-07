df3e6a17b1c1cd30e7839e10f073b38e9b3ed7c3
Stefany Lino <stefanylinosilva47@gmail.com>
2024-12-07 05:12:23 -0300

    Index - PHP

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </head>
 <body>
     <nav>
-        <div class="logo"><img src="Imagens/Logo.png" height="50px" alt="Logo"></div>
+        <div class="logo"><a href="home.php"><img src="Imagens/Logo.png" height="50px" alt="Logo"></a></div>
         <div class="nav-items">
             <a href="home.php"><i class="fa-solid fa-house"></i>&nbsp;HOME</a>
             <a href="index.php"><i class="fa-solid fa-folder"></i>&nbsp;Sistema de Cadastro</a>
